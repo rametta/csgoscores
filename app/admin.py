12a0260ba789e4c5052d4c1e3cddca7f64524518
Jason Rametta <Jason Rametta>
2016-08-24 22:05:53 -0400

    added Match model

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from .models import Map, Mode, Rank
+from .models import Map, Mode, Rank, Match
+
+
+class MatchAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'outcome', 'rounds_won', 'kills', 'deaths',
+        'kill_death_ratio','score','map','mode','date',)
+
 
 admin.site.register(Map)
 admin.site.register(Mode)
 admin.site.register(Rank)
+admin.site.register(Match, MatchAdmin)
+admin.site.site_header = 'CS:GO Scores'
+admin.site.site_title = 'CS:GO Scores'
--- app/models.py
+++ app/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+
 class Map(models.Model):
     title = models.CharField(max_length=64)
 
     def __str__(self):
         return self.title
+
 
 class Mode(models.Model):
     title = models.CharField(max_length=64)
@@ -12,8 +14,45 @@ class Mode(models.Model):
     def __str__(self):
         return self.title
 
+
 class Rank(models.Model):
     title = models.CharField(max_length=64)
 
     def __str__(self):
         return self.title
+
+
+class Match(models.Model):
+    rounds_won = models.PositiveSmallIntegerField(verbose_name='Rounds Won')
+    rounds_lost = models.PositiveSmallIntegerField(verbose_name='Rounds Lost')
+    kills = models.PositiveSmallIntegerField()
+    assists = models.PositiveSmallIntegerField()
+    deaths = models.PositiveSmallIntegerField()
+    mvps = models.PositiveSmallIntegerField(verbose_name='MVPs')
+    placement = models.PositiveSmallIntegerField()
+    score = models.PositiveSmallIntegerField()
+    date = models.DateField()
+    rank = models.ForeignKey(Rank, on_delete=models.CASCADE)
+    mode = models.ForeignKey(Mode, on_delete=models.CASCADE)
+    map = models.ForeignKey(Map, on_delete=models.CASCADE)
+    #TODO add fk to current user
+
+    def outcome(self):
+        if self.rounds_won > self.rounds_lost:
+            return "Win"
+        elif self.rounds_won < self.rounds_lost:
+            return "lost"
+        else:
+            return "Tie"
+
+    def round_win_loss_ratio(self):
+        return "{0:.2f}".format(self.rounds_won/self.rounds_lost)
+
+    def kill_death_ratio(self):
+        return "{0:.2f}".format(self.kills/self.deaths)
+
+    def __str__(self):
+        return self.outcome()
+
+    class Meta:
+        verbose_name_plural = 'Matches'
