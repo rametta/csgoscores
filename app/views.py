ae984b0711d8fd600fb28c59ef0c54d7341f26bc
Jason Rametta <Jason Rametta>
2016-08-24 23:12:43 -0400

    eddited templates

--- app/views.py
+++ app/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-from app.models import Map
+from app.models import Map, Match
 
 def index(request):
     all_maps = Map.objects.all()
     return render(request, 'index.html', {'maps': all_maps})
+
+def leaderboard(request):
+    stats = Match.objects.all()
+    return render(
+        request,
+        'leaderboard.html',
+        {'leaderboard': stats, 'page_title': 'Leaderboard'}
+    )
