cd0259fef0aecec0f8e33df3a0ccd551a6a0c154
Jason Rametta <rametta@outlook.com>
2016-08-25 21:14:33 -0400

    added steam template

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^leaderboard/$', views.leaderboard, name='leaderboard'),
     url(r'^new/$', views.add_match, name='add_match'),
-
+    url(r'^search/$', views.search, name='search'),
     # Registration
     url(r'^accounts/', include('registration.backends.hmac.urls')),
+
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 from .models import Match
 from .forms import MatchForm
 
@@ -14,7 +16,11 @@ def leaderboard(request):
         {'leaderboard': stats,'page_title': 'Leaderboard'}
     )
 
+def search(request):
+    return render(request, 'search.html', {})
 
+
+@login_required
 def add_match(request):
     if request.method == 'POST':
         form = MatchForm(data=request.POST)
@@ -26,4 +32,9 @@ def add_match(request):
     else:
         form = MatchForm()
 
-    return render(request, 'add_match.html', {'form': form})
+    return render(request, 'add_match.html', {'form': form, 'page_title': 'Add Score'})
+
+
+def logout(request):
+    logout(request, 'auth_login')
+    #return redirect('auth_login')
